c156f90a8a3f1240d50586f84d4eeed36f8c1db8
ntompkins <ntompkins@classworks.com>
2019-11-26 16:01:34 -0600

    Finished with all tasks.

--- app.js
+++ app.js
@@ -2,36 +2,45 @@ $(document).ready(function () {
 
   $('#btnSubmit').attr("disabled", true); // Disabled = false... TRUE.
   $('body').append('<div></div>');
+  $('body').append('<ul></ul>');
 
   $("#btnSubmit").on("click", function () {
-    // alert("Test!");
     alert($("input[type=text]").val());
     event.preventDefault();
-    $('div').append('<h2></h2>');
+    // $('div').append('<h2></h2>');
 
-    $('h2').append($("#inputText").val());
+    // $('h2').append($("#inputText").val());
 
-    $('h2').on("mouseover", function () {
-      $('h2').css("background-color", "yellow");
-      $('h2').css("border", "2px solid red");
+    // $('h2').on("mouseover", function () {
+    //   $('h2').css("background-color", "yellow");
+    //   $('h2').css("border", "2px solid red");
+    // })
+
+    $('ul').append('<li>' + $("#inputText").val() + '</li>');
+
+    $("li").click(function (e) {
+      let r = Math.floor(Math.random() * 255);
+      let g = Math.floor(Math.random() * 255);
+      let b = Math.floor(Math.random() * 255);
+      $(e.target).css("color", "rgb(" + r + ", " + g + ", " + b + ")");  // We could've also substituted the above "Math.floor" function calls in place of the variables: "r", "g", and "b". It works the same.
+
+    })
+
+    $("li").on("dblclick", function (e) {
+      e.target.remove();
     })
 
   })
 
-  $("#inputText").on("keyup", function () {
-    console.log('Input text has been changed!');
 
+  $("#inputText").on("keyup", function () {
     let textEntered = $("#inputText").val();
     if (textEntered.length > 0) {
-      // console.log(textEntered.length);
       $('#btnSubmit').attr("disabled", false);
     } else {
       $('#btnSubmit').attr("disabled", true);
     }
 
   })
-
-
-
 
 });
